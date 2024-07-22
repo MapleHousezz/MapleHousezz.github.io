ea466d1c88da597e2901a8eb8c153781fa163f61
MapleHousezz <2528833214@qq.com>
2024-07-22 10:16:06 +0800

    Site updated: 2024-07-22 10:16:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/19/index/"];function toRandomPost(){
+var posts=["2024/07/19/index/","2024/07/22/CNN/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
